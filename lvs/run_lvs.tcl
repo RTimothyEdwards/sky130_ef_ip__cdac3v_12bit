4caac728caa50f51d3e6016ab2c81073dcb8e2a5
Tim Edwards <tim@opencircuitdesign.com>
2024-06-12 14:37:18 -0400

    Corrected the name of the layout and schematic to match the project name.

--- lvs/run_lvs.tcl
+++ lvs/run_lvs.tcl
@@ -1,6 +1,6 @@
 # Tcl script to run LVS on the ICRG ultra-low-power comparator
 
-set project sky130_ef_ip__cdac12bit
+set project sky130_ef_ip__cdac3v_12bit
 
 if {[catch {set PDK_ROOT $::env(PDK_ROOT)}]} {set PDK_ROOT /usr/local/share/pdk}
 if {[catch {set PDK $::env(PDK)}]} {set PDK sky130A}
@@ -13,13 +13,13 @@ set setupfile ${techlibs}/netgen/sky130A_setup.tcl
 set hvlib ${reflibs}/sky130_fd_sc_hvl/spice/sky130_fd_sc_hvl.spice
 set hdlib ${reflibs}/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice
 
-set circuit1 [readnet spice ../netlist/layout/sky130_ef_ip__cdac12bit.spice]
+set circuit1 [readnet spice ../netlist/layout/${project}.spice]
 set circuit2 [readnet spice $hvlib]
 
 readnet spice $hdlib $circuit2
-readnet spice ../netlist/schematic/sky130_ef_ip__cdac12bit.spice $circuit2
+readnet spice ../netlist/schematic/${project}.spice $circuit2
 
 # debug on
 
-lvs "$circuit1 sky130_ef_ip__cdac12bit" "$circuit2 sky130_ef_ip__cdac12bit" \
-        $setupfile sky130_ef_ip__cdac12bit_comp.out
+lvs "$circuit1 ${project}" "$circuit2 ${project}" \
+        $setupfile ${project}_comp.out
